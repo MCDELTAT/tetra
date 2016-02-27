0d17a819f1c41a49446eade1cb9bfef52ae934bc
Aaron Chamberlain <aaronjchamberlain@gmail.com>
2016-02-27 13:45:57 -0800

    Added a working uploader

---
+++ proto2_web/experiments/file_upload/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var multer = require('multer'),
+		bodyParser = require('body-parser'),
+		path = require('path');
+
+var app = new express();
+app.use(bodyParser.json());
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//route for GET request
+app.get('/', function(req,res){
+	res.render('index');
+});
+
+app.post('/', multer({dest: './uploads/',limits: {fileSize: 1000000, files: 1}}).single('upl'), function(req,res){
+	console.log(req.body); //form fields
+	console.log(req.file); //form files
+	res.status(204).end();
+});
+
+var port = 3000;
+app.listen(port, function(){
+	console.log('listening on port '+port);
+});		
