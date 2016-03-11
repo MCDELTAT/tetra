f3c6503c7ec7ec670f40430412f4521439a9f13a
jmose27 <jmose27@gmail.com>
2016-03-11 01:04:25 -0800

    gestures to new branch

---
+++ proto2_web/js/parser.js
@@ -0,0 +1,81 @@
+var lineCount;
+
+function verifyObject(){
+	lineCount = data.data.length;
+	console.log("Line count is: ",lineCount);
+}	
+
+var graphMaxima = {
+	"xMin": 0,
+	"yMin": 0,
+	"zMin": 0,
+	"xMax": 0,
+	"yMax": 0,
+	"zMax": 0
+};
+
+//get the maxima/minima of each axis (dim)
+function getMinMax() {
+	for (var i=0; i<lineCount; i++){
+		//if the current lines value is less than the min, it is the new min.
+		if (data.data[i].dim1 < graphMaxima["xMin"]){
+			graphMaxima["xMin"] = data.data[i].dim1;
+			//console.log("New X-Minimum: ", graphMaxima["xMin"]);
+		}
+		if (data.data[i].dim1 > graphMaxima["xMax"]){
+			graphMaxima["xMax"] = data.data[i].dim1;
+			//console.log("New X-Maximum ", graphMaxima["xMax"]);
+		}
+		//compare the Y values to find min & max.
+		if (data.data[i].dim2 < graphMaxima["yMin"]){
+			graphMaxima["yMin"] = data.data[i].dim2;
+			//console.log("New Y-Minimum: ", graphMaxima["yMin"]);
+		}
+		if (data.data[i].dim2 > graphMaxima["yMax"]){
+			graphMaxima["yMax"] = data.data[i].dim2;
+			//console.log("New Y-Maximum ", graphMaxima["yMax"]);
+		}
+		//compare the Z values to find min & max.
+		if (data.data[i].dim3 < graphMaxima["zMin"]){
+			graphMaxima["zMin"] = data.data[i].dim3;
+			//console.log("New Z-Minimum: ", graphMaxima["zMin"]);
+		}
+		if (data.data[i].dim3 < graphMaxima["zMax"]){
+			graphMaxima["zMax"] = data.data[i].dim3;
+			//console.log("New Z-Minimum: ", graphMaxima["zMax"]);
+		}
+	}
+	console.log("The X-Minimum is: ",graphMaxima["xMin"]);
+	console.log("The X-Maximum is: ",graphMaxima["xMax"]);
+	console.log("The Y-Minimum is: ",graphMaxima["yMin"]);
+	console.log("The Y-Maximum is: ",graphMaxima["yMax"]);
+	console.log("The Z-Minimum is: ",graphMaxima["zMin"]);
+	console.log("The Z-Maximum is: ",graphMaxima["zMax"]);
+
+	return graphMaxima;
+}
+
+var speciesArray = []; //array that will contain the different species
+//function takes in data from above, and splits the different species into
+//different objects. These objects are then stored in an array.
+function createSpeciesObjects(){
+	var speciesName1 = "";
+	var speciesCount = -1;
+	//loop through parsedData Object, for each line, add to corresponding species obj.
+	for (var i=0; i<lineCount; i++){
+		//if the species name changes, create a new object for it.
+		if (speciesName1 != data.data[i].Organism){
+			speciesName1 = data.data[i].Organism; 
+			speciesArray.push(new Object())
+			speciesCount++; //start the count at zero.
+			console.log(typeof speciesName1);
+		}
+		//speciesArray[speciesCount] is object, as above, add data entry to that species object.
+		speciesArray[speciesCount][i] = data.data[i];
+	}
+	console.log("The contents of array are: \n",speciesArray[1]); //method access single species obj.
+	console.log("The contents of array are: \n",speciesArray[2]); //method access single species obj.
+	console.log("The value of speciesCount is: ",(speciesCount+1));
+
+	return speciesArray;
+}
--- proto2_web/js/scatter_plot.js
+++ proto2_web/js/scatter_plot.js
@@ -1,54 +1,85 @@
 //scene and camera
+var cameraControls;
+var group;
 
+var clock = new THREE.Clock();
 var scene = new THREE.Scene(); 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-var cameraControls;
-var clock = new THREE.Clock();
+camera.position.z = 75;
+
 //set up the renderer
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //set up the cube, apply face visibility to half of the faces
-var geometry = new THREE.BoxGeometry(50,50,50);
-materials = [
-	new THREE.MeshBasicMaterial({color: 0xeef5e1, side: THREE.DoubleSide}),
-	new THREE.MeshBasicMaterial({transparent: true, opacity: 0})
-];
-//graph consists of faces 2,3,6,7,10,11
-var faceCount = geometry.faces.length;
-for (var i=0; i<(faceCount); i++){
-	if (i==2||i==3||i==6||i==7||i==10||i==11){
-		geometry.faces[i].materialIndex = 0;
-	} else{
-		geometry.faces[i].materialIndex = 1;
-	}
-}
-var cube = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
-scene.add(cube);
-camera.position.z = 48.5;
-camera.position.x = 48.5;
-//camera.rotation.y = 0.529269912; //PI/8
+var geometry = new THREE.CubeGeometry(50,50,50);
+
+//color: 0xeef5e1,
+
+var materials = [
+       new THREE.MeshBasicMaterial({ //right
+           map: THREE.ImageUtils.loadTexture('textures/pps.png'),
+           transparent: true, 
+           side: THREE.DoubleSide,
+		   opacity: 0
+       }),
+       new THREE.MeshBasicMaterial({
+           map: THREE.ImageUtils.loadTexture('textures/pps.png'),
+           transparent: false, 
+           side: THREE.DoubleSide,
+		   opacity: 0
+       }),
+       new THREE.MeshBasicMaterial({
+           map: THREE.ImageUtils.loadTexture('textures/pps.png'),
+           transparent: true, 
+           side: THREE.DoubleSide,
+		   opacity: 0
+       }),
+       new THREE.MeshBasicMaterial({
+           map: THREE.ImageUtils.loadTexture('textures/pps.png'),
+           transparent: false, 
+           side: THREE.DoubleSide,
+		   opacity: 0
+       }),
+       new THREE.MeshBasicMaterial({ //left
+           //map: THREE.ImageUtils.loadTexture('textures/pps.png'),
+           transparent: true,
+           side: THREE.DoubleSide,
+           opacity: 0
+       }),
+       new THREE.MeshBasicMaterial({
+           map: THREE.ImageUtils.loadTexture('textures/pps.png'),
+           transparent: false, 
+           side: THREE.DoubleSide,
+		   opacity: 1
+       })
+    ];
+
+    graph = new THREE.Mesh(geometry, 
+    	new THREE.MeshFaceMaterial(materials)) ;
+
+    scene.add(graph);
 cameraControls = new THREE.TrackballControls(camera, renderer.domElement);
 cameraControls.target.set(0, 0, 0);
-//scene.add(camera);
-function animate(){
-	var delta = clock.getDelta();
-	requestAnimationFrame(animate);
-	cameraControls.update(delta);
-	renderer.render(scene,camera);
-	//stats.update();
+camera.position.z = 48.5;
+camera.position.x = 48.5;
+camera.rotation.y = 0.529269912; //PI/8
 
-}
+
+
 //function to generate spheres at coordinates
 //three.js Objects all have an incremental id
 //in this case, the spheres are id:4 onward (scene=1,cam=2,grid=3)
 var sphGeometry = new THREE.SphereGeometry(0.25,32,32); 
 var speciesMat1 = new THREE.MeshBasicMaterial({color: 0x84dbfc});
+var speciesMat2 = new THREE.MeshBasicMaterial({color: 0xfa515f});
+var speciesMat3 = new THREE.MeshBasicMaterial({color: 0x6bf35c});
+var speciesMaterials = [speciesMat1,speciesMat2,speciesMat3];
 var pointsStartIndex = 4;
 var tempObject;
-function createSphere(xCoor,yCoor,zCoor){
-	scene.add(new THREE.Mesh(sphGeometry,speciesMat1));
+function createSphere(species,xCoor,yCoor,zCoor){
+	scene.add(new THREE.Mesh(sphGeometry,speciesMaterials[species]));
 	tempObject = scene.getObjectById(pointsStartIndex,true);
 	tempObject.position.set(xCoor/10,yCoor/10,zCoor/10);
 	pointsStartIndex++; //increment the var so next point can be id'ed correctly
@@ -72,40 +103,23 @@ function changeVisible(startRange, stopRange){
 	}
 }
 
-//actual: get length of parsed species object, loop n.
-var species1 = {
-	contig1: {
-		xCoor: -152.71487,
-		yCoor: 198.21319,
-		zCoor: 25.004952
-	},
-	contig2: {
-		xCoor: -232.2761,
-		yCoor: 147.22785,
-		zCoor: -4.7544378
-	},
-	contig3: {
-		xCoor: -203.38871,
-		yCoor: 98.04291,
-		zCoor: 26.501083
-	},
-	contig4: {
-		xCoor: -188.01657,
-		yCoor: 130.2182,
-		zCoor: 46.68083
-	},
-	contig5: {
-		xCoor: -182.42861,
-		yCoor: 160.0382,
-		zCoor: 35.121653
-	}
-};
+function getSpeciesLength(speciesID){
+	var length = 0;
+	var length = Object.keys(speciesArray[speciesID]).length;
 
-createSphere(species1.contig1.xCoor,species1.contig1.yCoor,species1.contig1.zCoor);
-createSphere(species1.contig2.xCoor,species1.contig2.yCoor,species1.contig2.zCoor);
-createSphere(species1.contig3.xCoor,species1.contig3.yCoor,species1.contig3.zCoor);
-createSphere(species1.contig4.xCoor,species1.contig4.yCoor,species1.contig4.zCoor);
-createSphere(species1.contig5.xCoor,species1.contig5.yCoor,species1.contig5.zCoor);
+	console.log("The length of the selected species is: ",length);
+	return length;
+}
+
+function drawPoints (species, length){
+	//get the first parameter name of each speciesObject
+	var startIndex = Object.keys(speciesArray[species])[0]
+	console.log("The start index of "+species+" is: ",startIndex);
+	console.log("The range of drawing is from "+startIndex+" to "+(+startIndex+ +length));
+	for (var i=startIndex; i<(+startIndex + +length); i++){
+		createSphere(species,speciesArray[species][i].dim1,speciesArray[species][i].dim2,speciesArray[species][i].dim3);
+	}
+}
 
 //create a button to toggle on and off the species
 var species1Btn = document.createElement("BUTTON");
@@ -118,6 +132,18 @@ species1Btn.addEventListener("click", function(){changeVisible(4,8); });
 function render() {
 	requestAnimationFrame(render);
 	renderer.render(scene,camera);
+	//camera.position.y += .001;
 }
-//render();
+function animate() {
+    
+        var delta = clock.getDelta();
+
+        requestAnimationFrame(animate);
+        
+        cameraControls.update(delta);
+        
+        renderer.render(scene, camera);
+        
+}
+render();
 animate();
